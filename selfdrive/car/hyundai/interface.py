079d51ac6d6a57b053f58150df8d006850d223f2
Vehicle Researcher <>
2020-11-23 09:57:23 +0900

    openpilot v0.7.10 release

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -57,13 +57,13 @@ class CarInterface(CarInterfaceBase):
     #ret.lateralTuning.pid.kdBP, ret.lateralTuning.pid.kdV = [[0.], [0.]]
     ret.lateralTuning.init('lqr')
     ret.lateralTuning.lqr.scale = 1500
-    ret.lateralTuning.lqr.ki = 0.1
+    ret.lateralTuning.lqr.ki = 0.01
     ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
     ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
     ret.lateralTuning.lqr.c = [1., 0.]
     ret.lateralTuning.lqr.k = [-110., 451.]
     ret.lateralTuning.lqr.l = [0.33, 0.318]
-    ret.lateralTuning.lqr.dcGain = 0.027
+    ret.lateralTuning.lqr.dcGain = 0.0027
     ret.steerMaxV = [1.5]
     ret.steerMaxBP = [0.]
 
